4f297d371a4d8aaa36b754c50147829f56fa7c81
ozguvenc <123120253+ozguvenc@users.noreply.github.com>
2024-02-08 08:32:48 -0700

    Repo Upload Part-3

--- CryptRaider/Source/CryptRaider/Mover.h
+++ CryptRaider/Source/CryptRaider/Mover.h
@@ -23,6 +23,14 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(EditAnywhere)
+	FVector MoveOffset;
 
+	UPROPERTY(EditAnywhere)
+	float MoveTime = 4;
+
+	UPROPERTY(EditAnywhere)
+	bool ShouldMove = false;
 		
 };
